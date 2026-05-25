9c6cf786f8b9c995748d91a9fef3ba0ad1639132
AnthonyAndino <anthonyandino959@gmail.com>
2026-05-24 20:00:00 -0600

    Agregadas vistas de Asistencia y Nomina

--- backend/empleados/views.py
+++ backend/empleados/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics
-from .models import Empleado
-from .serializers import EmpleadoSerializer
+from .models import Empleado, Asistencia, Nomina
+from .serializers import EmpleadoSerializer, AsistenciaSerializer, NominaSerializer
 
 class EmpleadoListCreateAPIView(generics.ListCreateAPIView):
     queryset = Empleado.objects.all().order_by('idEmpleado')
@@ -9,3 +9,11 @@ class EmpleadoListCreateAPIView(generics.ListCreateAPIView):
 class EmpleadoRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Empleado.objects.all()
     serializer_class = EmpleadoSerializer
+
+class AsistenciaListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Asistencia.objects.all().order_by('-fecha', '-hora_entrada')
+    serializer_class = AsistenciaSerializer   
+     
+class NominaListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Nomina.objects.all().order_by('-fecha_pago')
+    serializer_class = NominaSerializer
